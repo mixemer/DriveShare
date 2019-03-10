ee11e9d297b32d9c2df7ecca6a5457f667191dcd
mixemer <mehmetshin96@gmail.com>
2019-03-10 11:31:15 -0400

    first commit

---
+++ DriveShare/Model/CustomCar.swift
@@ -0,0 +1,24 @@
+//
+//  CustomCar.swift
+//  DriveShare
+//
+//  Created by Mehmet Sahin on 3/10/19.
+//  Copyright © 2019 Mehmet Sahin. All rights reserved.
+//
+
+import UIKit
+
+class CustomCar: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
